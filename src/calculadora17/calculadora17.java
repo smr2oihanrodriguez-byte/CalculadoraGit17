e9be8b079dd10b559ac03060f9f476c6bae95bf2
Oihan <smr2.oihan,rodriguez@gmail.com>
2026-02-11 11:40:38 +0100

    Proyecto CalculadoraGIT17 clase Calculadora y metodo dividir

--- src/calculadora17/calculadora17.java
+++ src/calculadora17/calculadora17.java
@@ -9,5 +9,12 @@ public class calculadora17 {
 	public int restar(int a, int b) {
 		 return a - b;
 		 }
+	
+	public int multiplicar(int a, int b) {
+		 return a * b;
+		}
+		public double dividir(int a, int b) {
+		 return (double) a / b;
+		}
 }
 
