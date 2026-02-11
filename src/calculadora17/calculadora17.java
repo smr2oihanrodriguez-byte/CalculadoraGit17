0123d7af7a09018d56c5c7110a6d9f3915138bb7
Oihan <smr2.oihan,rodriguez@gmail.com>
2026-02-11 11:52:37 +0100

    Hemos calculado la division entre 0

--- src/calculadora17/calculadora17.java
+++ src/calculadora17/calculadora17.java
@@ -16,6 +16,14 @@ public class calculadora17 {
 		public double dividir(int a, int b) {
 		 return (double) a / b;
 		}
+		
+	public double dividir(int a, int b) {
+			 if (b == 0) {
+			 throw new IllegalArgumentException("No se puede dividir entre cero");
+			 }
+			 return (double) a / b;
+			}
+
 }
 
  
