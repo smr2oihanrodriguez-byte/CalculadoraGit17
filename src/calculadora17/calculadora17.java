cc0f8fbfbb24e19183b71b007c6bed4accbd4821
Oihan <smr2.oihan,rodriguez@gmail.com>
2026-02-11 12:00:14 +0100

    Proyecto Calculadora17 clase calculadora y metodo potencia

--- src/calculadora17/calculadora17.java
+++ src/calculadora17/calculadora17.java
@@ -23,6 +23,18 @@ public class calculadora17 {
 			 }
 			 return (double) a / b;
 			}
+	
+	public int potencia(int base, int exponente) {
+		 int resultado = 1;
+		 for (int i = 0; i < exponente; i++) {
+		 resultado *= base;
+		 }
+		 return resultado;
+		}
+		public int modulo(int a, int b) {
+		 return a % b;
+		}
+
 
 }
 
